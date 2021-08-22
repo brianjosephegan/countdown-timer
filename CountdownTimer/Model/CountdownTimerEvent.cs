6f83337d6ee65afd7b8af5ae76ed6389d58ee74d
Brian Egan <brianjoseph.egan@gmail.com>
2021-08-22 11:47:20 -0700

    Input Form
    Add input form and model to configure the event to countdown to. Currently just logging the values on submission of the form.

---
+++ CountdownTimer/Model/CountdownTimerEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountdownTimer.Model
+{
+    public class CountdownTimerEvent
+    {
+        public string Name { get; set; }
+
+        public DateTime DateTime { get; set; }
+    }
+}
