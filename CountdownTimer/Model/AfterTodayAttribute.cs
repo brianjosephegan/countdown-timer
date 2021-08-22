5c3015766d35ea7f677be6199c3add80d9923e8e
Brian Egan <brianjoseph.egan@gmail.com>
2021-08-22 13:37:04 -0700

    Validation and Labels
    Add validation and labels to the form.

---
+++ CountdownTimer/Model/AfterTodayAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountdownTimer.Model
+{
+    public class AfterTodayAttribute : ValidationAttribute
+    {
+        private string errorMessage = "The Date field is required to be after today.";
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var date = (DateTime)value;
+
+            if (date.Date <= DateTime.Now.Date)
+            {
+                return new ValidationResult(errorMessage);
+            }
+            else
+            {
+                return ValidationResult.Success;
+            }
+        }
+    }
+}
--- CountdownTimer/Model/CountdownTimerEvent.cs
+++ CountdownTimer/Model/CountdownTimerEvent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,16 @@ namespace CountdownTimer.Model
 {
     public class CountdownTimerEvent
     {
+        public CountdownTimerEvent()
+        {
+            Date = DateTime.Today.AddDays(1);
+        }
+
+        [Required]
         public string Name { get; set; }
 
-        public DateTime DateTime { get; set; }
+        [AfterToday]
+        [Required]
+        public DateTime Date { get; set; }
     }
 }
