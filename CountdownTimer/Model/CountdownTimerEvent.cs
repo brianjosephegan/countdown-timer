e323e872448a0b036087e55d573431916e15429b
Brian Egan <brianjoseph.egan@gmail.com>
2021-08-22 15:01:30 -0700

    Countdown Commences
    Add ability to start the countdown to the specified date. Indicate this in the webpage and update each second.

--- CountdownTimer/Model/CountdownTimerEvent.cs
+++ CountdownTimer/Model/CountdownTimerEvent.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Timers;
 using System.Threading.Tasks;
 
 namespace CountdownTimer.Model
 {
     public class CountdownTimerEvent
     {
+        public event EventHandler SecondCountdownEvent;
+
         public CountdownTimerEvent()
         {
             Date = DateTime.Today.AddDays(1);
@@ -19,5 +22,25 @@ namespace CountdownTimer.Model
         [AfterToday]
         [Required]
         public DateTime Date { get; set; }
+
+        public TimeSpan TimeLeft { get; private set; }
+
+        public void StartCountdown()
+        {
+            TimeLeft = Date.Subtract(DateTime.Now);
+
+            var timer = new Timer();
+            timer.Interval = TimeSpan.FromSeconds(1).TotalMilliseconds;
+            timer.Elapsed += Timer_Elapsed;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+            timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            TimeLeft = TimeLeft.Subtract(TimeSpan.FromSeconds(1));
+            SecondCountdownEvent?.Invoke(this, new EventArgs());
+        }
     }
 }
